6e48ba7ab8acdd873b7ea15f99e8005096596701
labricecat <labricecat@duck.com>
2026-06-17 12:39:20 +0200

    adjusted ports

--- config/general.py
+++ config/general.py
@@ -1,18 +1,18 @@
-SDA_PIN = 19
-SCL_PIN = 18
+SDA_PIN = 33
+SCL_PIN = 32
 
 WIFI_SSID = "meowphone"
 WIFI_PASSWORD = "labricecat"
 
-MQTT_ENABLED = True
-MQTT_USER = "mqttuser"
-MQTT_BROKER = "192.168.194.117"
+MQTT_ENABLED = False
+MQTT_USER = "htl"
+MQTT_BROKER = "sc.htl-kaindorf.at"
 MQTT_PORT = 1883
-MQTT_PASSWORD = "password"
+MQTT_PASSWORD = "welc0m3"
 
 TOPIC_LOCATION = "top"
 
-SOLENOID = 0
+SOLENOID = 34
 SOLENOID_COMMAND_TOPIC = TOPIC_LOCATION + "/solenoid/set"
 SOLENOID_STATUS_TOPIC = TOPIC_LOCATION + "/solenoid/status"
 
